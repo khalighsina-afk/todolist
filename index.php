13c69abe1adbba0ed584d6c73f28608716b6754f
khalighsina-afk <khalighsina@gmail.com>
2026-06-30 18:28:54 +0330

    message

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     include ("database.php");
 
     $title  = "my first task";
-    $desc   = "testing my first task";
+    $desc   = "testing my firat task";
 
     $sql = "INSERT INTO tasks(title, descr) VALUES ('$title', '$desc')";
     mysqli_query($conn, $sql);
